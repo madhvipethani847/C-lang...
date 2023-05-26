9a5bf286cd6120b6facd66bd9e690f1227a9541f
madhvipethani847 <134709511+madhvipethani847@users.noreply.github.com>
2023-05-26 22:41:47 +0530

    Add files via upload

---
+++ main (7).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    
+    printf(" _  _  _  _\n");
+    printf("|          |\n");
+    printf("|          |\n");
+    printf("|  dhuvi   |\n");
+    printf("|          |\n");
+    printf("|          |\n");
+    printf(" _  _  _  _\n");
+    
+    return 0;
+}
